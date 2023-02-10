c5a5c8302bf43699b1651369e22aaf29b5c2fa08
vedansh kumar <56169665+vedanshk@users.noreply.github.com>
2023-02-10 23:31:07 +0530

    added mongo driver

--- maxcoin/services/backend/MongoBackend.js
+++ maxcoin/services/backend/MongoBackend.js
@@ -1,23 +1,88 @@
 /* eslint-disable no-useless-constructor */
 /* eslint-disable class-methods-use-this */
 /* eslint-disable no-empty-function */
-const CoinAPI = require('../CoinAPI');
-
+const CoinAPI = require("../CoinAPI");
+const { MongoClient } = require("mongodb");
 class MongoBackend {
-
   constructor() {
     this.coinAPI = new CoinAPI();
+    this.mongoUrl = "mongodb://localhost:37017/maxcoin";
+    this.client = null;
+    this.collection = null;
   }
 
-  async connect() {}
+  async connect() {
+    const mongoClient = new MongoClient(this.mongoUrl);
 
-  async disconnect() {}
+    this.client = await mongoClient.connect();
+    this.collection = this.client.db("maxcoin").collection("values");
 
-  async insert() {}
+    return this.client;
+  }
 
-  async getMax() {}
+  async disconnect() {
+    if (this.client) {
+      return this.client.close();
+    }
 
-  async max() {}
+    return false;
+  }
+
+  async insert() {
+    const data = await this.coinAPI.fetch();
+    const documents = [];
+
+    Object.entries(data.bpi).forEach((entry) => {
+      documents.push({
+        data: entry[0],
+        value: entry[1],
+      });
+    });
+
+    return this.collection.insertMany(documents);
+  }
+
+  async getMax() {
+    return this.collection.findOne({}, { sort: { value: 1 } });
+  }
+
+  async max() {
+    console.log(`Connection to MongoDB`);
+
+    console.time("mongodb-connect");
+    const client = await this.connect();
+    if (client) {
+      console.log("Successfully connect to MongoDB");
+    } else {
+      throw new Error("Connecting to Mongodb failed");
+    }
+
+    console.timeEnd("mongodb-connect");
+    console.log("Inserting into Mongodb");
+    console.time("mongodb-insert");
+
+    const insertResult = await this.insert();
+
+    console.timeEnd("mongodb-insert");
+
+    console.log(`Inserted ${insertResult.result} documents into MongoDB`);
+
+
+    console.log("Query max");
+    console.time("mongodb-find")
+    const doc = await this.getMax();
+    console.timeEnd("mongodb-find")
+    console.log(`Disconnecting  to MongoDB`);
+
+    console.time("mongodb-disconnect");
+    await this.disconnect();
+    console.timeEnd("mongodb-disconnect");
+
+    return {
+      data:doc.data,
+      value:doc.value,
+    };
+  }
 }
 
 module.exports = MongoBackend;
